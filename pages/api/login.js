a52a31dcaeedfeee191ccf184fb4904f4dfa2ae5
Nikita <nikita_dubyk@mail.ru>
2022-02-17 10:06:12 +0300

    remove jwt, added Iron and fixed login logic

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,12 @@
 import { users } from './data/users'
-import { setTokenCookie } from '../../lib/auth-cookies'
+import { setLoginSession } from '../../lib/auth'
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { password, email } = req.body
         const user = users.find(user => user.email === email)
         let result
-
         if (user) result = bcrypt.compareSync(password, user.password)
 
         if (user === undefined) {
@@ -16,12 +14,7 @@ export default async function handler(req, res) {
         } else if (user.email === email && !result) {
             res.status(401).json({ errorMessage: 'Wrong password' })
         } else if (user.email === email && result) {
-            const token = jwt.sign({ id: user.id }, 'secret_key', {
-                expiresIn: '5h',
-            })
-
-            setTokenCookie(res, token)
-
+            await setLoginSession(res, user.id)
             res.status(200).json({})
         }
     }
